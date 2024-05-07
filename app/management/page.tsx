f13708860801db63dcdf19050a9091b0752cf27b
Hashen Udara <106765093+HashenUdara@users.noreply.github.com>
2024-05-08 03:02:46 +0530

    added autth

--- app/management/page.tsx
+++ app/management/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
   return (
     <>
       <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
+        <h1 className="text-lg font-semibold md:text-2xl">
+          Trading Platform Administration
+        </h1>
       </div>
       <div
         className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
@@ -13,12 +16,14 @@ const Page = () => {
       >
         <div className="flex flex-col items-center gap-1 text-center">
           <h3 className="text-2xl font-bold tracking-tight">
-            You have no products
+            Development Mode
           </h3>
           <p className="text-sm text-muted-foreground">
-            You can start selling as soon as you add a product.
+            This Page is still under development
           </p>
-          <Button className="mt-4">Add Product</Button>
+          <Link href={"/management/users"}>
+            <Button className="mt-4">Continue to Demo</Button>
+          </Link>
         </div>
       </div>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,43 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function LoginForm() {
   return (
-    <section className='py-24'>
-      <div className='container'>
-        <h1 className='text-3xl font-bold'>Clerk starter</h1>
-      </div>
-    </section>
-  )
+    <div className=" flex justify-center h-screen items-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Administration</CardTitle>
+          <CardDescription>
+            Please login as an admin to get access for Management Dashboard
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4"></CardContent>
+        <CardFooter className="flex justify-center items-center">
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button className="w-full">Sign in</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <Link href={"/management/users"}>
+              <Button className="mt-4 w-full">
+                Continue to Admin Dashboard
+              </Button>
+            </Link>
+          </SignedIn>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
